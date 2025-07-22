e1b44997911d640dba07a253af8012b26e84de80
OgGG-rj <ggwithrj@gmail.com>
2025-07-22 08:59:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (data) => {
+    io.emit('chat message', data);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
